c63b547fd0fa3e81fc947ed9373be6955c7c78fb
prishpon <prishpon@gmail.com>
2025-12-03 16:53:34 +0100

    Firebase config fix

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDG10gwR2TB85e8HWL13hCH2R4GZwHcjdY",
-  authDomain: "dutcharticlesapp.firebaseapp.com",
-  projectId: "dutcharticlesapp",
-  storageBucket: "dutcharticlesapp.firebasestorage.app",
-  messagingSenderId: "294220643612",
-  appId: "1:294220643612:web:2f4c554ce912e6fc528ed9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
